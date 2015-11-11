a146b8306a46d7f8679b90014310b662cc1dc700
zorgle <zzorgle@gmail.com>
2015-11-10 21:22:32 -0500

    Code cleanup in Profile.cs

--- AC Account Manager/AC Account Manager/Profile.cs
+++ AC Account Manager/AC Account Manager/Profile.cs
@@ -9,7 +9,7 @@ namespace AC_Account_Manager
         private readonly Dictionary<string, CharacterSetting> _characterSettings = new Dictionary<string, CharacterSetting>();
         public void AddCharacterSetting(string accountName, string serverName, string chosenCharacter)
         {
-            string key = GetKey(accountName: accountName, serverName: serverName);
+            string key = GetCharacterKey(accountName: accountName, serverName: serverName);
             var setting = new CharacterSetting
                 {
                     AccountName = accountName,
@@ -18,14 +18,9 @@ namespace AC_Account_Manager
                 };
             _characterSettings[key] = setting;
         }
-        public void AddCharacterSetting(CharacterSetting charSetting)
-        {
-            string key = GetKey(charSetting);
-            _characterSettings[key] = charSetting;
-        }
         public string GetCharacterSetting(string accountName, string serverName)
         {
-            string key = GetKey(accountName: accountName, serverName: serverName);
+            string key = GetCharacterKey(accountName: accountName, serverName: serverName);
             if (_characterSettings.ContainsKey(key))
             {
                 return _characterSettings[key].ChosenCharacter;
@@ -39,13 +34,9 @@ namespace AC_Account_Manager
         {
             return _characterSettings.Values;
         }
-        private string GetKey(string accountName, string serverName)
+        private string GetCharacterKey(string accountName, string serverName)
         {
             return accountName.ToUpper() + ":" + serverName.ToUpper();
-        }
-        private string GetKey(CharacterSetting charSetting)
-        {
-            return GetKey(accountName: charSetting.AccountName, serverName: charSetting.ServerName);
         }
     }
 }
