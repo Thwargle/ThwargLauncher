c6b786ec1dc595d42ca1da7ad8a762ce08b17a51
Thwargle <thwargle.newberry@gmail.com>
2015-12-17 18:22:50 -0500

    Make profile name editable in Choose profile

--- AC Account Manager/ThwargLauncher/ProfileManagement/ProfileChoiceViewModel.cs
+++ AC Account Manager/ThwargLauncher/ProfileManagement/ProfileChoiceViewModel.cs
@@ -1,17 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace ThwargLauncher
 {
-    class ProfileChoiceViewModel
+    class ProfileChoiceViewModel : INotifyPropertyChanged
     {
-        private Profile _profile;
+        private readonly Profile _profile;
         public ProfileChoiceViewModel(Profile profile)
         {
             _profile = profile;
         }
-        public string Name { get { return _profile.Name; } }
+        public string Name
+        {
+            get { return _profile.Name; }
+            set
+            {
+                var profileManager = new ProfileManager();
+                bool renamed = profileManager.RenameProfile(_profile.Name, value);
+                if (renamed)
+                {
+                    OnPropertyChanged("CurrentProfileName");
+                }
+            }
+        }
         public string Description { get { return _profile.Description; } }
         public int ActiveAccounts { get { return _profile.ActiveAccountCount; } }
         public int ActiveServers { get { return _profile.ActiveServerCount; } }
@@ -21,5 +32,11 @@ namespace ThwargLauncher
         {
             return (date.HasValue ? date.Value : defval);
         }
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
