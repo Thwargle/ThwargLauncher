fa8f95312a887709c7445b08497159c13bfaab9c
Thwargle <thwargle.newberry@gmail.com>
2015-12-17 18:17:04 -0500

    Change ChooseProfileViewModel to have a collection of view models instead of a data view, and move profile code into new ProfileManagement subfolder

---
+++ AC Account Manager/ThwargLauncher/ProfileManagement/ChooseProfileViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ThwargLauncher
+{
+    class ChooseProfileViewModel
+    {
+        private readonly ObservableCollection<ProfileChoiceViewModel> _profileModels = new ObservableCollection<ProfileChoiceViewModel>();
+        public ObservableCollection<ProfileChoiceViewModel> AllProfiles { get { return _profileModels; } }
+        public ChooseProfileViewModel(List<Profile> profiles)
+        {
+            foreach (var profile in profiles)
+            {
+                var profileModel = new ProfileChoiceViewModel(profile);
+                _profileModels.Add(profileModel);
+            }
+        }
+    }
+}
---
+++ AC Account Manager/ThwargLauncher/ProfileManagement/ProfileChoiceViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ThwargLauncher
+{
+    class ProfileChoiceViewModel
+    {
+        private Profile _profile;
+        public ProfileChoiceViewModel(Profile profile)
+        {
+            _profile = profile;
+        }
+        public string Name { get { return _profile.Name; } }
+        public string Description { get { return _profile.Description; } }
+        public int ActiveAccounts { get { return _profile.ActiveAccountCount; } }
+        public int ActiveServers { get { return _profile.ActiveServerCount; } }
+        public DateTime LastLaunch { get { return PopulateDate(_profile.LastLaunchedDate, DateTime.MinValue); } }
+
+        private DateTime PopulateDate(DateTime? date, DateTime defval)
+        {
+            return (date.HasValue ? date.Value : defval);
+        }
+    }
+}
