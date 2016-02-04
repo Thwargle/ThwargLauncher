8e1b780b5c5ec197bbc49f927b1300465c6dba55
zorgle <zzorgle@gmail.com>
2016-02-03 21:30:11 -0500

    Upload new ReserveWebServiceAddress program

---
+++ AC Account Manager/ReserveWebServiceAddress/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace ReserveWebServiceAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string accountName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            string netshArgs = string.Format(
+                "http add urlacl url=http://+:33000/ user={0}",
+                accountName);
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("netsh", netshArgs);
+
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
+
+            Process.Start(procStartInfo);
+        }
+    }
+}
