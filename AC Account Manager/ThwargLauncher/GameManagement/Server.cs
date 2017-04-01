1458275638909d6b551912f95e606af1f5808407
zorgle <zzorgle@gmail.com>
2017-04-01 18:51:35 -0400

    Move ServerManager to its own file

--- AC Account Manager/ThwargLauncher/GameManagement/Server.cs
+++ AC Account Manager/ThwargLauncher/GameManagement/Server.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using ThwargLauncher.UtilityCode;
 
 namespace ThwargLauncher
 {
@@ -161,38 +160,4 @@ namespace ThwargLauncher
             return address;
         }
     }
-    public class ServerManager
-    {
-        public static List<Server.ServerItem> ServerList = new List<Server.ServerItem>();
-        public static bool IsLoaded;
-
-        public void LoadServerLists()
-        {
-            var phatServers = (new GameManagement.PhatACServerLister()).loadPhatServers();
-            var aceServers = (new GameManagement.AceServerLister()).loadACEServers();
-            foreach (var serverItem in phatServers.DistinctBy(p => p.GetHashCode()))
-            {
-                AddOrUpdateServer(serverItem);
-            }
-            foreach (var serverItem in aceServers.DistinctBy(p => p.GetHashCode()))
-            {
-                AddOrUpdateServer(serverItem);
-            }
-            IsLoaded = true;
-        }
-        private void AddOrUpdateServer(Server.ServerItem server)
-        {
-            var existing = ServerList.FirstOrDefault(s => s.GetHashCode() == server.GetHashCode());
-            if (existing != null)
-            {
-                // Currently we don't update because our GUI doesn't support editing existing servers
-            }
-            else
-            {
-                ServerList.Add(server);
-            }
-        }
-    }
-
-
 }
---
+++ AC Account Manager/ThwargLauncher/GameManagement/ServerManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ThwargLauncher.UtilityCode;
+
+namespace ThwargLauncher
+{
+    public class ServerManager
+    {
+        public static List<Server.ServerItem> ServerList = new List<Server.ServerItem>();
+        public static bool IsLoaded;
+
+        public void LoadServerLists()
+        {
+            var phatServers = (new GameManagement.PhatACServerLister()).loadPhatServers();
+            var aceServers = (new GameManagement.AceServerLister()).loadACEServers();
+            foreach (var serverItem in phatServers.DistinctBy(p => p.GetHashCode()))
+            {
+                AddOrUpdateServer(serverItem);
+            }
+            foreach (var serverItem in aceServers.DistinctBy(p => p.GetHashCode()))
+            {
+                AddOrUpdateServer(serverItem);
+            }
+            IsLoaded = true;
+        }
+        private void AddOrUpdateServer(Server.ServerItem server)
+        {
+            var existing = ServerList.FirstOrDefault(s => s.GetHashCode() == server.GetHashCode());
+            if (existing != null)
+            {
+                // Currently we don't update because our GUI doesn't support editing existing servers
+            }
+            else
+            {
+                ServerList.Add(server);
+            }
+        }
+    }
+}
