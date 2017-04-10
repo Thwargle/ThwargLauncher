a1787b7442c60fbd3ea594838d56e6548b816034
zorgle <zzorgle@gmail.com>
2017-04-10 10:02:47 -0400

    MagFilter: add disposal to new ThwargInventory class

--- AC Account Manager/Mag-Software/Mag-Filter/ThwargInventory.cs
+++ AC Account Manager/Mag-Software/Mag-Filter/ThwargInventory.cs
@@ -10,10 +10,35 @@ namespace MagFilter
     class ThwargInventory
     {
         private Dictionary<int, string> _items = new Dictionary<int, string>();
+        private bool disposed;
 
         public ThwargInventory()
         {
             CoreManager.Current.ItemSelected += Current_ItemSelected;
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            // Use SupressFinalize in case a subclass
+            // of this type implements a finalizer.
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            // If you need thread safety, use a lock around these 
+            // operations, as well as in your methods that use the resource.
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    CoreManager.Current.ItemSelected -= Current_ItemSelected;
+                }
+                // Indicate that the instance has been disposed.
+                disposed = true;
+            }
         }
 
         private void Current_ItemSelected(object sender, ItemSelectedEventArgs e)
