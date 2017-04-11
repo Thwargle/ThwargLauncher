09e4c7ad2baf014b564fbdd3b5eb57c94758b83e
Thwargle <thwargle.newberry@gmail.com>
2017-04-11 00:04:47 -0400

    Updated to 4.6.2.0 and created a new release.

--- AC Account Manager/ThwargLauncher/Properties/AssemblyInfo.cs
+++ AC Account Manager/ThwargLauncher/Properties/AssemblyInfo.cs
@@ -45,11 +45,11 @@ using System.Windows;
 //
 //      Major Version
 //      Minor Version 
-//      Build Number
+//      Build NumberIsReadOnly="{Binding IsPublished}"
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.6.1.0")]
-[assembly: AssemblyFileVersion("4.6.1.0")]
+[assembly: AssemblyVersion("4.6.2.0")]
+[assembly: AssemblyFileVersion("4.6.2.0")]
