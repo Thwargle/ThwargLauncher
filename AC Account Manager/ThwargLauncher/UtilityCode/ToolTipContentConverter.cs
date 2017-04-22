dd0444b1759ce3fb1e08a354282d5b55f9ffd8d1
Zorgle <zorgle@users.noreply.github.com>
2017-04-22 02:07:59 -0400

    EditCharacters window: Change list to grid and add detail pane at bottom

---
+++ AC Account Manager/ThwargLauncher/UtilityCode/ToolTipContentConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Globalization;
+
+namespace ThwargLauncher
+{
+    [ValueConversion(typeof(object), typeof(string))]
+    public class ToolTipContentConverter : MarkupExtension, IValueConverter
+    {
+        public ToolTipContentConverter() { }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
