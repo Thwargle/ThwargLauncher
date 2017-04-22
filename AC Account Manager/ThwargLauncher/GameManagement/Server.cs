38f3568939dede04f13992ded2deb5a37752d9b2
Zorgle <zorgle@users.noreply.github.com>
2017-04-22 18:20:45 -0400

    Move AddressParser into its own file

--- AC Account Manager/ThwargLauncher/GameManagement/Server.cs
+++ AC Account Manager/ThwargLauncher/GameManagement/Server.cs
@@ -204,30 +204,4 @@ namespace ThwargLauncher
             PropertyChanged.Raise(this, propertyName);
         }
     }
-    public static class AddressParser
-    {
-        public class Address
-        {
-            public string Ip { get; set; }
-            public int Port { get; set; }
-        }
-        public static Address Parse(string text)
-        {
-            var address = new Address();
-            int index = text.IndexOf(':');
-            if (index > 0)
-            {
-                address.Ip = text.Substring(0, index);
-                if (index < text.Length - 1)
-                {
-                    int val = 0;
-                    if (int.TryParse(text.Substring(index + 1), out val))
-                    {
-                        address.Port = val;
-                    }
-                }
-            }
-            return address;
-        }
-    }
 }
