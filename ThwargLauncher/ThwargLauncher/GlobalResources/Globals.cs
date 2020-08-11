6d48f888c92956a3db577493d9b102c606fd19d5
thwargle <thwargle.newberry@gmail.com>
2020-08-11 14:04:53 -0400

    add globals file to repo

---
+++ ThwargLauncher/ThwargLauncher/GlobalResources/Globals.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ThwargLauncher.GlobalResources
+{
+    class Globals
+    {
+        public static bool IsSimple {  get { return Properties.Settings.Default.LastUsedSimpleLaunch; } }
+        public static bool NeverKillClients { get { return Properties.Settings.Default.NeverKillClients; } }
+    }
+}
