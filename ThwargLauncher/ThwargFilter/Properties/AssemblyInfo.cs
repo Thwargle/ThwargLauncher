9fc35b88cef5fb4df63d58e32b33cf324c9772b9
thwargle <thwargle.newberry@gmail.com>
2021-11-17 14:07:47 -0500

    Bumped build version to 7 and built new installer

--- ThwargLauncher/ThwargFilter/Properties/AssemblyInfo.cs
+++ ThwargLauncher/ThwargFilter/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Thwargle Games")]
 [assembly: AssemblyProduct("ThwargFilter")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.8.0.3")]
-[assembly: AssemblyFileVersion("6.8.0.3")]
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
